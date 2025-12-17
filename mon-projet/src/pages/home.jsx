832640ae7ddf9ff3b6c9b5ad10326f4992839c52
gregory adenor <gregoryadenor@epitech.eu>
2025-12-17 08:22:48 +0400

    test d'affichage

--- mon-projet/src/pages/home.jsx
+++ mon-projet/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import "./home.css"
 import { Start } from "../components/start"
-import { Test } from "../components/testfigth"
+// import { Test } from "../components/testfigth"
+import { Test } from "../components/testGreg"
 
 export function Home() {
     return(
